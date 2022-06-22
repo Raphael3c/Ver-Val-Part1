a1a98e852e78857ee1d2fed0a86ca0f1263dc566
Raphael Cavalcante da Silva <phael.ufc@gmail.com>
2022-06-21 21:43:57 -0300

    atualizacao

--- src/controllers/reschedule.ts
+++ src/controllers/reschedule.ts
@@ -2,8 +2,15 @@ import { Schema, model, mongo, models } from 'mongoose';
 import { IService } from "../models/Service";
 import { Request, Response } from "express";
 import "dotenv/config";
+import { validateTax } from '../utils/validateTax';
 
 export const reschedule = async (req: Request, res: Response) => {
+
+	if(validateTax(req.body.taxId)){
+		res.end(JSON.stringify({Success: false, mensage: "Cpf invalido"}))
+		return
+	}
+
 	const serviceSchema = new Schema<IService>({
 		taxId: String,
 		serviceName: String,
