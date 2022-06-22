bb30a8e5c54eebb8c67409426c3ae74887f59513
Raphael Cavalcante da Silva <phael.ufc@gmail.com>
2022-06-21 21:41:32 -0300

    Atualizações

--- src/controllers/createService.ts
+++ src/controllers/createService.ts
@@ -1,9 +1,18 @@
 import { Schema, models, model } from 'mongoose'
 import { IService } from '../models/Service'
 import { Request, Response } from 'express'
+import { validateTax } from '../utils/validateTax'
 import 'dotenv/config'
 
 export const createService = async (req: Request, res: Response) => {
+
+  const taxId = req.body.taxId;
+
+  if(!validateTax(taxId)){
+	  res.end(JSON.stringify({Success: false, mensage: "CPF invalido"}))
+    return
+  }
+
 	const serviceSchema = new Schema<IService>({
     taxId: String,
     employeeName: String,
@@ -11,18 +20,17 @@ export const createService = async (req: Request, res: Response) => {
 		value: Number,
 		duration: String,
 	})
-
-  const createdServices =  models.Service || model<IService>('createdservices', serviceSchema)
+  
+  const createdServices = models.createdservices || model<IService>('createdservices', serviceSchema)
   
   const data: IService = {
-    taxId: "88855522210",
-    employeeName: "Vanessa Camargo",
-    description: "Disponivel",
-    value: 35,
-    duration: 30,
+    taxId: req.body.taxId,
+    employeeName: req.body.employeeName,
+    description: req.body.description,  
+    value: req.body.value,
+    duration: req.body.duration,
     dateRegistered: new Date().toISOString(),
   }
-
 
   createdServices.create(data);
 
